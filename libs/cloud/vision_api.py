9d01877076c97c271b9421a574842f8f8d21f2bd
subho406 <subhojeetpramanik@gmail.com>
2018-02-05 21:47:57 +0530

    vision api sample

---
+++ libs/cloud/vision_api.py
@@ -0,0 +1,14 @@
+"""Netra Vision GCP Library
+
+    Authors:
+            Subhojeet Pramanik
+            Prateek Singh
+
+
+"""
+
+
+def label_captions(img_file):
+    return None
+
+
---
+++ libs/cloud/vision_api_test.py
@@ -0,0 +1,6 @@
+from . import vision_api
+
+
+if __name__ == '__main__':
+    # Do something
+    a=10
